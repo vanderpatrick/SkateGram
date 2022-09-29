a05c8d9f0e9c02b7e9bf2cc0c535e6194d24259d
vanderpatrick <patrick123321123@icloud.com>
2022-09-29 10:53:03 +0000

    Configure cloudinary credentials

--- skategram/settings.py
+++ skategram/settings.py
@@ -147,3 +147,10 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CLOUDINARY = {
+      'cloud_name': 'volendam',  
+      'api_key': '683414431453265',  
+      'api_secret': '3anVzkjZpTMOY8W72RR93K29r0k',
+}
