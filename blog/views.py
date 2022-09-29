f635f461b78aacd0cd76d6da54441d7e2bf7abe9
vanderpatrick <patrick123321123@icloud.com>
2022-09-29 08:15:48 +0000

    create and add content to base.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from .models import Post, TutorialPost, Profile, Comment
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required 
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.contrib import messages
+# from .forms import UserRegister, UpdateUserProfile, UserUpadateForm, PostForm
+from tinymce import models as tinymce_models
 # Create your views here.
+
+
+def landing(request):
+    return render(request, 'landing.html')
