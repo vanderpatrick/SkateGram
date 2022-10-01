5f7ee7822a8c952d0afc3c449d1f2b7f13d73aab
vanderpatrick <patrick123321123@icloud.com>
2022-10-01 12:12:54 +0000

    edit comment mdoel

--- blog/models.py
+++ blog/models.py
@@ -21,6 +21,7 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('home')
+        
     def save(self, *args, **kwargs):
         super().save()
         
@@ -47,7 +48,8 @@ class Profile(models.Model):
 
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    date = models.DateTimeField(auto_now_add=True)
     content = models.TextField(max_length=200)
 
     def __str__(self):
